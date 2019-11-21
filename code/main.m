0279c67d2db1eaa61cc05f17ac09cf6daabce879
Miguel Altuve <miguelaltuve@gmail.com>
2019-11-21 22:13:57 +0000

    Edited main.m, Dockerfile

--- code/main.m
+++ code/main.m
@@ -2,7 +2,17 @@
 
 %addpath(pwd);
 
-wfdbdemo
 %singlechannel_detection_performance_main;
 
 %multichannel_detector_performance_main;
+
+[old_path]=which('rdsamp'); if(~isempty(old_path)) rmpath(old_path(1:end-8)); end
+wfdb_url='https://physionet.org/physiotools/matlab/wfdb-app-matlab/wfdb-app-toolbox-0-10-0.zip';
+[filestr,status] = urlwrite(wfdb_url,'wfdb-app-toolbox-0-10-0.zip');
+unzip('wfdb-app-toolbox-0-10-0.zip');
+cd mcode
+addpath(pwd)
+savepath
+
+
+wfdbdemo
